6389695d2d00d49dc9a7e0b8b773c751ffdc1b64
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-06-24 20:25:15 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import '../node_modules/normalize.css/normalize.css'
-
-// import '@fortawesome/fontawesome-free/js/brands.js'
-// import '@fortawesome/fontawesome-free/js/solid.js'
-// import '@fortawesome/fontawesome-free/js/fontawesome.js'
-
 import 'material-icons/iconfont/material-icons.css';
-
 import './sass/main.scss';
 
 import './js/theme.js'
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,52 +1,8 @@
 import eventsTemples from '../templates/events.hbs'
 import munuEvents from '../menu.json'
-// console.log(munuEvents);
-// console.log(typeof munuEvents);
-// console.log(munuEvents.id);
 
 const menuList = document.querySelector('.js-menu');
 menuList.innerHTML = eventsTemples((munuEvents));
-// console.log(menuList);
-
-// const indigMas = munuEvents.map(({ingredients}) => {
-      // return ingredients
-      // console.log(ingredients)
-// })
-
-// console.log(indigMas);
-
-// for (indigMas )
-
-// for(var i=0; i < country.languages.length; i++)
-//     document.write(country.languages[i] + "<br/>");
-
-// indigMas.forEach(ingredient => {
-//    console.log(ingredient);
-// })
-
-   // .array.forEach(element => {
-      
-   // });
-   
-// munuEvents.forEach(ingredients)
-
-
-// const galleryMarkup = createGalleryMarkup(munuEvents);
-// console.log(galleryMarkup);
-
-// function createGalleryMarkup(munuEvents) {
-//    return munuEvents.map(({ingredients}) => {
-//       // console.log(ingredient)
-//       return `
-//       <li class="tag-list__item">
-//         ${ingredients} 
-//       </li>`
-//    })
-//          .join("\n");
-// }
-
-// const li = document.querySelector('.tag-list')
-// li.insertAdjacentHTML('beforeend', galleryMarkup);
 
 
 
