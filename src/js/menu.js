1c02a70058d8db1a236ec32aed9a04dd401aa899
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-06-24 13:07:02 +0300

    fix

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,26 +1,30 @@
 import eventsTemples from '../templates/events.hbs'
 import munuEvents from '../menu.json'
-console.log(munuEvents);
+// console.log(munuEvents);
 
 const menuList = document.querySelector('.js-menu');
 menuList.innerHTML = eventsTemples((munuEvents));
-
-// console.log(munuEvents); 
-
+console.log(menuList);
 
 
 
-// const galleryMarkup = createGalleryMarkup(menuList);
 
-// function createGalleryMarkup(menuList) {
-//    return menuList.map(({ingredients}) => {
+// const galleryMarkup = createGalleryMarkup(munuEvents);
+// console.log(galleryMarkup);
+
+// function createGalleryMarkup(munuEvents) {
+//    return munuEvents.map(({ingredients}) => {
+//       // console.log(ingredient)
 //       return `
-//       <li class="tag-list__item"">
+//       <li class="tag-list__item">
 //         ${ingredients} 
 //       </li>`
 //    })
-//       .join("");
-
+//          .join("\n");
 // }
 
-// menuList.insertAdjacentHTML('beforeend', galleryMarkup);
+// const li = document.querySelector('.tag-list')
+// li.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+
